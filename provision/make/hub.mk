e424bbecf176ffbf6671bfd19dfc2c44f50b11ae
@slovacus <slovacus@gmail.com>
2019-12-28 15:45:28 -0500

    chore: implement action for build image (#10)

--- provision/make/hub.mk
+++ provision/make/hub.mk
@@ -6,13 +6,13 @@ hub: hub.help
 hub.help:
 	@echo '    Hub:'
 	@echo ''
-	@echo '        hub.build         build {image}, one example: make hub.build version={{3.8.0-slim}}'
-	@echo '        hub.push          push image to docker hub, example make hub.push version={{3.8.0.slim}}'
+	@echo '        hub.build         build $(IMAGE), one example: make hub.build version={{3.8.0-slim}}'
+	@echo '        hub.push          push $(IMAGE) to docker hub, example make hub.push version={{3.8.0.slim}}'
 	@echo ''
 
 hub.build:
 	@echo " =====> Building $(IMAGE):${version}..."
-	@if [ -z ${version} ]; then \
+	if [ -z ${version} ]; then \
 		$(docker-build) $(IMAGE):latest ;\
 	else \
 		$(docker-build) $(IMAGE):${version} images/${version} ;\
@@ -20,7 +20,7 @@ hub.build:
 
 hub.push:
 	@echo " =====> Push $(IMAGE):${version}..."
-	@if [ -z ${version} ]; then \
+	if [ -z ${version} ]; then \
 		$(docker-push) $(IMAGE):latest ;\
 	else \
 		$(docker-push) $(IMAGE):${version} ;\
