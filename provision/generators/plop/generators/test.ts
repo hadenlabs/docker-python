e3a927571cbc5dfdb0cfc1723f10c4de32db1d0e
@slovacus <slovacus@gmail.com>
2021-04-16 07:32:26 -0500

    chore: implement generator plop (#17)

---
+++ provision/generators/plop/generators/test.ts
@@ -0,0 +1,45 @@
+import { Actions, PlopGeneratorConfig } from 'node-plop'
+import slugify from 'slugify'
+import * as path from 'path'
+import { baseGeneratorPath, baseTemplatesPath, pathExists, pathMake } from '../utils'
+
+export enum TestPrompNames {
+  'testName' = 'testName',
+  'testImage' = 'testImage',
+}
+
+type Answers = { [P in TestPrompNames]: string }
+
+const testPath = path.join(baseGeneratorPath, 'test')
+
+export const testGenerator: PlopGeneratorConfig = {
+  description: 'add an path to test',
+  prompts: [
+    {
+      type: 'input',
+      name: TestPrompNames.testName,
+      message: 'What should it be test?',
+      default: 'basic',
+    },
+    {
+      type: 'input',
+      name: TestPrompNames.testImage,
+      message: 'What should it be image?',
+      default: '3.8.0-slim',
+    },
+  ],
+  actions: (data) => {
+    const answers = data as Answers
+
+    const actions: Actions = []
+
+    actions.push({
+      type: "add",
+      templateFile: `${baseTemplatesPath}/test/test.append.hbs`,
+      path: `${testPath}/docker_python_${slugify(answers.testName, '_')}_${slugify(answers.testImage, '_')}_test.go`,
+      abortOnFail: true,
+    })
+
+    return actions
+  },
+}
